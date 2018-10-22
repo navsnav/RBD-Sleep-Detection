b6a968ac6d389678753b67661ccdd3f2b1465444
Fernando Andreotti <fernando.andreotti@eng.ox.ac.uk>
2018-10-22 17:02:15 +0100

    Re-adding process_classification_results2

--- RBD_Detection_main.m
+++ RBD_Detection_main.m
@@ -117,7 +117,7 @@ indices = crossvalind('Kfold',rbd_group, folds);
 % Apply cross fold validation for automated sleep staging followed by RBD
 % detection using established metrics and new metrics
 disp(['Initiating ',num2str(folds),' fold cross validation with ',num2str(n_trees),' trees.']);
-[Auto_SS_Results,RBD_New_Results,EMG_Est_Results,EMG_Auto_New_Results,EMG_Auto_Est_Results,All_Confusion] = RBD_Detection2(Sleep_table_Pre,Sleep_Struct,rbd_group,indices,folds,SS_Features,EMG_est_feats,EMG_feats,ECG_feats,n_trees,view_results,print_figures,print_folder,save_data,outfilename,display_flag);
+[Auto_SS_Results,RBD_New_Results,EMG_Est_Results,EMG_Auto_New_Results,EMG_Auto_Est_Results,All_Confusion] = RBD_Detection(Sleep_table_Pre,Sleep_Struct,rbd_group,indices,folds,SS_Features,EMG_est_feats,EMG_feats,ECG_feats,n_trees,view_results,print_figures,print_folder,save_data,outfilename,display_flag);
 
 
 
