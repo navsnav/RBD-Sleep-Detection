82b53568d8b0e87fdda032200def3bb14f42e412
navsnav <navsnav@gmail.com>
2018-10-22 21:05:43 +0100

    Updated
    Updated

--- subfunctions/print_annotated_vs_auto.m
+++ subfunctions/print_annotated_vs_auto.m
@@ -1,13 +1,12 @@
-function print_annotated_vs_auto(EMG_Table_Names,EMG_feats,rbd,EMG_Metric,EMG_Auto_Metric,print_figures,print_folder)
+function print_annotated_vs_auto(EMG_Table_Names,EMG_feats,EMG_Metric,EMG_Auto_Metric,print_figures,print_folder)
 % This function plots the RBD metrics derived from manually annotated 
 % and automatically classified sleep 
 %
 % Inputs:
 %  EMG_Table_Names    - names of metrics 
 %  EMG_feats   - index of metrics
-%  rbd  - RBD status (0: HC, 1: RBD)
-%  EMG_Metric  - Matrix of RBD metrics
-%  EMG_Auto_Metric  - Matrix of RBD metrics derivied from automatically
+%  EMG_Metric  - Table of RBD metrics
+%  EMG_Auto_Metric  - Table of RBD metrics derivied from automatically
 %                     classified sleep stages
 %  print_figures  - flag to print/save figures
 %  print_folder  -  folder to save figures
@@ -41,16 +40,18 @@ function print_annotated_vs_auto(EMG_Table_Names,EMG_feats,rbd,EMG_Metric,EMG_Au
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-for q=1:length(EMG_feats)
+rbd = EMG_Metric.RBD==1;
 
-    feat_name = cell2mat(EMG_Table_Names(EMG_feats(q)));
+for p=1:length(EMG_feats)
+    q = EMG_feats(p);
+    feat_name = cell2mat(EMG_Table_Names(q));
 
-    fig_num(q) = figure;
-    R = corrcoef(EMG_Metric(:,q),EMG_Auto_Metric(:,q),'rows','pairwise');
-    plot(EMG_Metric(~rbd,q),EMG_Auto_Metric(~rbd,q),'bo');
+    fig_num(p) = figure;
+    R = corrcoef(table2array(EMG_Metric(:,q)),table2array(EMG_Auto_Metric(:,q)),'rows','pairwise');
+    plot(table2array(EMG_Metric(~rbd,q)),table2array(EMG_Auto_Metric(~rbd,q)),'bo');
     hold on;
-    max_val = ceil(max(EMG_Metric(:,q)));
-    plot(EMG_Metric(rbd,q),EMG_Auto_Metric(rbd,q),'ro');
+    max_val = ceil(max(table2array(EMG_Metric(:,q))));
+    plot(table2array(EMG_Metric(rbd,q)),table2array(EMG_Auto_Metric(rbd,q)),'ro');
     plot(0:max_val/10:max_val,0:max_val/10:max_val,'k--');
     % plot(ones(1,11)*0.9,0:0.1:1,'k--');
     title([feat_name,' Comparison (Correlation Coeff: ',num2str(R(1,2),3),')'], 'Interpreter', 'none');
@@ -60,8 +61,8 @@ for q=1:length(EMG_feats)
     ylim([0 max_val]);
     legend({'HC','RBD'},'Location','northwest');
 
-    if (print_figures), saveas(fig_num(q),strcat(print_folder,'\',['Summary_All_',feat_name,'_Actual_Vs_Manual']),'epsc'), end  
-    if (print_figures), saveas(fig_num(q),strcat(print_folder,'\',['Summary_All_',feat_name,'_Actual_Vs_Manual']),'fig'), end  
+    if (print_figures), saveas(fig_num(p),strcat(print_folder,'\',['Summary_All_',feat_name,'_Actual_Vs_Manual']),'epsc'), end  
+    if (print_figures), saveas(fig_num(p),strcat(print_folder,'\',['Summary_All_',feat_name,'_Actual_Vs_Manual']),'fig'), end  
     
     
 end
@@ -72,12 +73,12 @@ end
     per_idx = find(strcmp(EMG_Table_Names(EMG_feats),'MAD_Per'));
 
     fig_num(q+1) = figure;
-    R = corrcoef(max(EMG_Metric(:,dur_idx),EMG_Metric(:,per_idx)),max(EMG_Auto_Metric(:,dur_idx),EMG_Auto_Metric(:,per_idx)),'rows','pairwise');
-    plot(max(EMG_Metric(~rbd,dur_idx),EMG_Metric(~rbd,per_idx)),max(EMG_Auto_Metric(~rbd,dur_idx),EMG_Auto_Metric(~rbd,per_idx)),'bo');
+    R = corrcoef(max(table2array(EMG_Metric(:,dur_idx)),table2array(EMG_Metric(:,per_idx))),max(table2array(EMG_Auto_Metric(:,dur_idx)),table2array(EMG_Auto_Metric(:,per_idx))),'rows','pairwise');
+    plot(max(table2array(EMG_Metric(~rbd,dur_idx)),table2array(EMG_Metric(~rbd,per_idx))),max(table2array(EMG_Auto_Metric(~rbd,dur_idx)),table2array(EMG_Auto_Metric(~rbd,per_idx))),'bo');
     hold on;
-    max_val = ceil(max(EMG_Metric(:,dur_idx)));
+    max_val = ceil(max(table2array(EMG_Metric(:,dur_idx))));
 %     plot(EMG_Metric(rbd,q),EMG_Auto_Metric(rbd,q),'ro');
-    plot(max(EMG_Metric(rbd,dur_idx),EMG_Metric(rbd,per_idx)),max(EMG_Auto_Metric(rbd,dur_idx),EMG_Auto_Metric(rbd,per_idx)),'ro');
+    plot(max(table2array(EMG_Metric(rbd,dur_idx)),table2array(EMG_Metric(rbd,per_idx))),max(table2array(EMG_Auto_Metric(rbd,dur_idx)),table2array(EMG_Auto_Metric(rbd,per_idx))),'ro');
     
     plot(0:max_val/10:max_val,0:max_val/10:max_val,'k--');
     % plot(ones(1,11)*0.9,0:0.1:1,'k--');
